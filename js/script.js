260fb8330a8d316f25c6cff9f4f9485684912e80
raisa-farzana <raisafarzana5502@gmail.com>
2025-04-02 16:20:05 +0600

    update js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const timerDisplay = document.getElementById('timer');
+const startBtn = document.getElementById('start-Btn');
+const stopBtn = document.getElementById('stop-Btn');
+
+
+
+
+startBtn.addEventListener('click',() => {
+    
+    
+});
+
+stopBtn.addEventListener('click', () => {
+    
+    
+});
